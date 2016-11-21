269aab4f52a8d63a9c5b066535c0cc453654a833
rohan mishra <rohmish26@gmail.com>
2016-11-21 22:44:47 +0530

    Added premitive sync. (DOES NOT WORK)

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import privVars as keys
-#from privVars import *
+import pycurl
 
 print (keys.weatherApiKey)
+
+#set storage and API link
+returnData = None
+link = 'https://api.darksky.net/forecast/' + keys.weatherApiKey +'/19.211,72.874'
+
+# set instance
+connection = pycurl.Curl()
+
+#set options
+connection.setopt(connection.URL, link)
+connection.setopt(connection.WRITEDATA, returnData)
+
+# perform cURL operation and close connection
+connection.perform()
+connection.close()
+
+print (returnData)
