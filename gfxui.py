9d319d847cf4be9aa570db039a75993da9e6e9b4
rohan mishra <rohmish26@gmail.com>
2016-12-10 15:28:34 +0530

    Added docs

--- gfxui.py
+++ gfxui.py
@@ -1,8 +1,67 @@
 import os
 import sys
 import time
-import gobject
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk,GObject
 import subprocess
 
 #TODO: Add UI.
+
+class UI(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Wether Options")
+        self.set_border_width(8)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        self.progressbar = Gtk.ProgressBar()
+        vbox.pack_start(self.progressbar, True, True, 0)
+
+        button = Gtk.CheckButton("Show text")
+        button.connect("toggled", self.on_show_text_toggled)
+        vbox.pack_start(button, True, True, 0)
+
+        button = Gtk.CheckButton("Wind Activity")
+        button.connect("toggled", self.on_activity_mode_toggled)
+        vbox.pack_start(button, True, True, 0)
+
+        self.timeout_id = GObject.timeout_add(70, self.on_timeout, None)
+        self.activity_mode = False
+
+    def on_show_text_toggled(self, button):
+        show_text = button.get_active()
+        if show_text:
+            text = "Getting wether info..."
+        else:
+            text = None
+        self.progressbar.set_text(text)
+        self.progressbar.set_show_text(show_text)
+
+    def on_activity_mode_toggled(self, button):
+        self.activity_mode = button.get_active()
+        if self.activity_mode:
+            self.progressbar.pulse()
+        else:
+            self.progressbar.set_fraction(0.0)
+
+    def on_timeout(self, user_data):
+        if self.activity_mode:
+            self.progressbar.pulse()
+        else:
+            new_value = self.progressbar.get_fraction() + 0.008
+
+            if new_value > 1:
+                new_value = 0
+
+            self.progressbar.set_fraction(new_value)
+
+        # As this is a timeout function, return True so that it
+        # continues to get called
+        return True
+mainwin = UI()
+mainwin.connect("delete-event", Gtk.main_quit)
+mainwin.show_all()
+Gtk.main()
